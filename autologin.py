d7b754b625bda7aa5c1f88d77e22cf85980ff005
bigb123 <piotr.palka123@gmail.com>
2018-09-20 00:05:52 +1000

    Added verification of the connection with the outside world

--- autologin.py
+++ autologin.py
@@ -15,6 +15,8 @@ from subprocess import getoutput
 import re
 from time import sleep
 from threading import Thread
+from urllib.request import urlopen
+from urllib.error import URLError
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -38,6 +40,22 @@ def captive_portal_killer(how_often = 1):
         sleep(how_often)
 
 
+'''
+Returns True if network connection is established.
+'''
+def connected_to_network():
+    try:
+        urlopen('http://www.freenom.world/').read()
+    except URLError:
+        return False
+
+    return True
+
+
+# If there is connection to outside world - there's no point to execute script.
+if connected_to_network():
+    exit(0)
+
 argument_parser = ArgumentParser()
 argument_parser.add_argument('username',
     action='store',
@@ -51,31 +69,25 @@ args = argument_parser.parse_args()
 # Get Wi-fi name and run it only if it is Telstra Air.
 if re.compile('Current Wi-Fi Network: ').sub('', getoutput('/usr/sbin/networksetup -getairportnetwork en0')) == 'Telstra Air':
 
-    # Check if the "Captive portal" window is available. If it is that means 
-    # we need to log in to Telstra Air. Other way there's no point to execute
-    # script.
-    if ( '' == getoutput('ps ax | grep "Captive Network Assistant" | grep -v "grep"')):
-        exit(0)
-
     # Kill the Captive Portal. It blocks all the network traffic.
     captive_killer_thread = Thread(target=captive_portal_killer)
     captive_killer_thread.daemon = True
     captive_killer_thread.start()
 
-    # Open browser
+    # Open browser.
     driver = webdriver.Safari()
     driver.get('https://www.telstra.com.au/airconnect#/main')
 
-    # Wait till page is fully loaded ('Log in' button must be visible)'
+    # Wait till page is fully loaded ('Log in' button must be visible)'.
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/p[2]/button')))
 
-    # Fill the form and log in
+    # Fill the form and log in.
     driver.find_element_by_id('username').send_keys(args.username)
     driver.find_element_by_id('password').send_keys(args.password)
     driver.find_element_by_xpath('//*[@id="loginForm"]/div/p[2]/button').click()
 
-    # Wait till next page is loaded ('Return to home' button must be visible) and
-    # quit the browser
+    # Wait till next page is loaded ('Return to home' button must be visible)
+    # and quit the browser.
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[11]/div/a/form/input')))
 
     driver.quit()
